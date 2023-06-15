cd25f71972c8bbe1efe0a02826f7d943598aeaf3
ashleytrollinger <ashley@trollingers.com>
2023-06-15 19:36:26 -0400

    auth.js in utils done

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//Make a post
+//Delete a post
+//Update a post
