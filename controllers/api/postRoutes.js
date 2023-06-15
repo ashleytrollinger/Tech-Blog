645031601dd053d1d6d7cdca4b1d1f03c23553ea
ashleytrollinger <ashley@trollingers.com>
2023-06-15 19:59:24 -0400

    postRoutes done

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,5 +3,55 @@ const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 //Make a post
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            // [{
+            //   "title": "",
+            //   "content": ""
+            // }]
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 //Delete a post
+router.delete('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.seession.user_id,
+            },
+        });
+        if (!projectData) {
+            res.status(404).json({ message: 'No project found to delete.' });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 //Update a post
+router.put('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No project found to update' })
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
