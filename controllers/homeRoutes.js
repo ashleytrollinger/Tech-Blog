961c9f5a55bcf3f096c7b1219864de901a6bc083
ashleytrollinger <ashley@trollingers.com>
2023-06-15 20:53:29 -0400

    blah

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
-const router =  require('express').Router();
-const { Post, User, Comment} = require('../models');
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -12,15 +12,16 @@ router.get('/', async (req, res) => {
                 },
             ],
         });
-    const posts = postData.map((post) => post.get({ plain: true }));
+        const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('homepage', {
-        posts,
-        logged_in: req.session.logged_in
-    });
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
     } catch (err) {
         res.status(500).json(err);
     }
 
 });
 
+module.exports = router;
