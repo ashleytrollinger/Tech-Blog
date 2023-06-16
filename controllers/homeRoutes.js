1fbd6cf4eac45de74f5e72814156e24abcb5410f
ashleytrollinger <ashley@trollingers.com>
2023-06-15 21:15:29 -0400

    homeRoutes done

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,4 +24,60 @@ router.get('/', async (req, res) => {
 
 });
 
+router.get('/post/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['comment', 'date'],
+                    include: {
+                        model: User,
+                        attributes: ['name'],
+                    }
+                }
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('login');
+})
+
 module.exports = router;
