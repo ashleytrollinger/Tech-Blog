db6ad3b973787fcab1bd968e1f17032bae4e3be4
ashleytrollinger <ashley@trollingers.com>
2023-06-16 21:20:56 -0400

    fixing homeorutes so it should work. Next is to figure out the handlebars fill ins because they are not loading in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,6 +25,29 @@ router.get('/homepage', async (req, res) => {
 
 });
 
+router.get('/', async (req, res) => {
+
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
 router.get('/post/:id', withAuth, async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
